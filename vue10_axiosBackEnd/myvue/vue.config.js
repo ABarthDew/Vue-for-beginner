abbc1d15367245992532ae7a5586389680a242b3
nicky0419@daum.net <matricks1924>
2019-11-20 17:55:29 +0900

    commit

--- vue10_axiosBackEnd/myvue/vue.config.js
+++ vue10_axiosBackEnd/myvue/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // options...
   devServer: {
     port: 9999,
-    // host:"locker.tpne.co.kr" //hosts에 등록 후 사용가능(호스트 변경 후 사용가능),
+    // host:"locker.tpne.co.kr" //hosts(system32..)에 등록 후 사용가능(호스트 변경 후 사용가능),
     // https:true //보안통신 <-> false(http)
     proxy: {//프론트에서 콜을 했을 때 백엔드로 갈 수 있도록 연결해주는 설정
       '^/api': {

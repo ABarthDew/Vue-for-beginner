2e93ab33c184cf1f2d900bea7dacf6ef0fcb35a4
nicky0419@daum.net <matricks1924>
2019-11-21 17:29:52 +0900

    vue router

---
+++ vue11_router/my-router/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div id="test">test</div>
+    <br />
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <!-- <router-view name="..."></router-view> -->
+    rrrrr
+    <router-link to="/my-login">로그인</router-link>&nbsp;&nbsp;&nbsp;
+    <router-link to="/my-join">가입</router-link>&nbsp;&nbsp;&nbsp;
+    <a href="#" @click.prevent="goHome">처음으로</a>
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  methods: {
+    goHome() {
+      this.$router.push('/my-login');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

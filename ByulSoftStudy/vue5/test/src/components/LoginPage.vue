a1a4502ae57836ea5c804f8519031de5a4777925
nicky0419@daum.net <matricks1924>
2019-12-19 11:40:30 +0900

    commit

---
+++ ByulSoftStudy/vue5/test/src/components/LoginPage.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+            <el-form-item label="password" prop="pass">
+                <el-input type="text" v-model="ruleForm.pass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="Confirm" prop="checkPass">
+                <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
+                <el-button @click="resetForm('ruleForm')">Reset</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            const validatePass = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('Please input the password'));
+                } else {
+                    if (this.ruleForm.checkPass !== '') {
+                        this.$refs.ruleForm.validateField('checkPass');
+                    }
+                    callback();
+                }
+            };
+            const validatePass2 = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('Please input the password again'));
+                } else if (value !== this.ruleForm.pass) {
+                    callback(new Error('Two inputs don\'t match!'));
+                } else {
+                    callback();
+                }
+            };
+            return {
+                ruleForm: {
+                    pass: '',
+                    checkPass: ''
+                },
+                rules: {
+                    pass: [
+                        { validator: validatePass, trigger: 'blur' }
+                    ],
+                    checkPass: [
+                        { validator: validatePass2, trigger: 'blur' }
+                    ]
+                }
+            };
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
+        }
+    }
+</script>
---
+++ ByulSoftStudy/vue5/test/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login">
+        <h1>This is an Login page</h1>
+       <Login></Login>
+    </div>
+</template>
+
+<script>
+    import Login from '@/components/Login.vue'
+    export default {
+        name:'login',
+        components:{
+            Login
+        }
+    }
+</script>
